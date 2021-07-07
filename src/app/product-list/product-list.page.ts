ed76305488a08a671c675d4bb2a9bc19661abf2a
karvannan <karvannan@netmobix.com>
2021-07-07 20:34:13 +0530

    ends with products-list

--- src/app/product-list/product-list.page.ts
+++ src/app/product-list/product-list.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PickerController } from "@ionic/angular";
+import { PickerOptions } from "@ionic/core";
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.page.html',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListPage implements OnInit {
 
+  animals: string[] = ["Active produts", "Inactive produts"];
   sample=[{'name':'Apple','product':'01',remaining:120,total:120,isActive:true ,remainings:'0'},
           {'name':'Bringal','product':'02',remaining:20,total:120,isActive:false, remainings:'0'},
           {'name':'Orange','product':'03',remaining:20,total:120,isActive:true ,remainings:'0'},
@@ -15,9 +17,39 @@ export class ProductListPage implements OnInit {
           
 
 ]
-  constructor() { }
+  constructor(private pickerController: PickerController) { }
 
   ngOnInit() {
   }
+  async showPicker() {
+    let options: PickerOptions = {
+      buttons: [
+        {
+          text: "Cancel",
+          role: 'cancel'
+        },
+        {
+          text:'Ok',
+          handler:(value:any) => {
+            console.log(value);
+          }
+        }
+      ],
+      columns:[{
+        name:'Animals',
+        options:this.getColumnOptions()
+      }]
+    };
 
+    let picker = await this.pickerController.create(options);
+    picker.present()
+  }
+
+  getColumnOptions(){
+    let options = [];
+    this.animals.forEach(x => {
+      options.push({text:x,value:x});
+    });
+    return options;
+  }
 }
